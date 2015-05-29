8590a0f95648c601a37e1e8bf1cc2c9316e72996
Irfan Mahfudz Guntur <tutacare@gmail.com>
2015-05-29 17:20:58 +0800

    add customer model

---
+++ database/migrations/2015_05_29_074713_create_customers_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customers', function(Blueprint $table)
+		{
+			$table->increments('id');
+			$table->string('name', 100);
+			$table->string('email', 30);
+			$table->string('phone_number', 20);
+			$table->string('address', 255);
+			$table->string('city', 20);
+			$table->string('state', 30);
+			$table->string('zip', 10);
+			$table->string('comment', 255);
+			$table->string('company_name', 100);
+			$table->string('account', 20);
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customers');
+	}
+
+}
