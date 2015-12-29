fdeef61c11e96a3cbdabc1e0ad94132177fa9483
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2015-12-29 01:57:00 -0400

    Report Translations.

---
+++ resources/lang/es/report-receiving.php
@@ -0,0 +1,20 @@
+<?php
+
+return [
+
+    'reports' => 'Reportes',
+    'receivings_report' => 'Reporte de Compras',
+    'grand_total' => 'TOTAL',
+    'receiving_id' => 'ID',
+    'date' => 'Fecha',
+    'items_received' => 'Items Comprados',
+    'received_by' => 'Recibido por',
+    'supplied_by' => 'Proveedor',
+    'total' => 'Total',
+    'payment_type' => 'Tipo de Pago',
+    'comments' => 'Comentarios',
+    'detail' => 'Detalle',
+    'item_id' => 'ID',
+    'item_name' => 'Producto:',
+    'item_received' => 'Cantidad',
+];
---
+++ resources/lang/es/report-sale.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    'reports' => 'Reportes',
+    'sales_report' => 'Reporte de Ventas',
+    'grand_total' => 'TOTAL',
+    'grand_profit' => 'GANANCIA',
+    'sale_id' => 'ID',
+    'date' => 'Fecha',
+    'items_purchased' => 'Items Vendidos',
+    'sold_by' => 'Vendido por',
+    'sold_to' => 'Cliente',
+    'total' => 'Total',
+    'profit' => 'Ganancia',
+    'payment_type' => 'Tipo de Pago',
+    'comments' => 'Comentarios',
+    'detail' => 'Detalle',
+    'item_id' => 'ID',
+    'item_name' => 'Producto:',
+    'quantity_purchase' => 'Cantidad',
+];
