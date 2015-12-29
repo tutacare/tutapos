64402789eb9546acc105f707bf8ba13c689ef5d6
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2015-12-29 01:48:57 -0400

    Translations.

---
+++ resources/lang/es/receiving.php
@@ -0,0 +1,27 @@
+<?php
+
+return [
+
+    'item_receiving' => 'Compra de Productos',
+    'search_item' => 'Buscar Producto:',
+    'invoice' => 'Recibo',
+    'employee' => 'Empleado',
+    'payment_type' => 'Tipo de Pago',
+    'supplier' => 'Proveedor',
+    'item_id' => 'ID',
+    'item_name' => 'Producto',
+    'cost' => 'Costo',
+    'quantity' => 'Cantidad',
+    'total' => 'Total',
+    'amount_tendered' => 'Monto Recibido',
+    'comments' => 'Comentarios',
+    'grand_total' => 'TOTAL:',
+    'submit' => 'Finalizar Compra',
+    //struk
+    'receiving_id' => 'Receiving ID',
+    'item' => 'Producto',
+    'price' => 'Precio',
+    'qty' => 'Cantidad',
+    'print' => 'Imprimir',
+    'new_receiving' => 'Nueva Compra',
+];
