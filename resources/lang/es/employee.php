c5b00bd9a4be49a936bce0bb7bda7e5d82cb2c08
Javier Rojas Blum <javier.rojas.blum@gmail.com>
2015-12-29 02:07:27 -0400

    Translations.

---
+++ resources/lang/es/employee.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+
+    'list_employees' => 'Listado de Empleados',
+    'new_employee' => 'Nuevo Empleado',
+    'person_id' => 'ID',
+    'name' => 'Nombre',
+    'email' => 'Email',
+    'password' => 'Contraseña',
+    'confirm_password' => 'Confirmar Contraseña',
+    'submit' => 'Guardar',
+    'edit' => 'Editar',
+    'delete' => 'Borrar',
+    'update_employee' => 'Actualizar Empleado'
+];
